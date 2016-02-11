3f87b13ffab4ea82f1c4ff386ba5ea56bdd36f0b
Niru Maheswaranathan <nirum@stanford.edu>
2016-02-10 20:28:14 -0800

    fixes install script for the case where the user doesn't have a styledir

--- install.py
+++ install.py
@@ -3,4 +3,13 @@ import shutil
 import matplotlib as mpl
 
 stylelib = os.path.join(mpl.get_configdir(), 'stylelib')
+
+# in case you don't have a stylelib directory yet
+try:
+    os.mkdir(stylelib)
+
+except FileExistsError:
+    pass
+
+# copy the files into the stylelib directory
 [shutil.copy(f, stylelib) for f in os.listdir('.') if f.endswith('.mplstyle')]
